e4868c18741d809b77ae9bd8f2a409fe98c62b9c
Galax27 <costin_tapean@yahoo.com>
2015-01-21 11:50:57 +0200

    Update Seed.cpp

--- MedicalMP/MedicalMP/Seed.cpp
+++ MedicalMP/MedicalMP/Seed.cpp
@@ -11,5 +11,17 @@ using namespace std;
 
 void Seed(Context* context)
 {
+	context->Patients.push_back(new Patient(100, 45, "Andrei", "Antoni", false));
+	context->Patients.push_back(new Patient(101, 38, "Alexandru", "Mateescu", false));
+	context->Patients.push_back(new Patient(102, 43, "Bogdan", "Musat", false));
+	context->Patients.push_back(new Patient(103, 65, "Berta", "Bolini", true));
+	context->Patients.push_back(new Patient(104, 29, "Bianca", "Rotaru", false));
+	context->Patients.push_back(new Patient(105, 18, "Colin", "Draganu", false));
+	context->Patients.push_back(new Patient(106, 23, "Costel", "Vasile", false));
+	context->Patients.push_back(new Patient(107, 55, "Dan", "Caciula", false));
+	context->Patients.push_back(new Patient(108, 53, "Daniela", "Matache", true));
+	context->Patients.push_back(new Patient(109, 47, "Darius", "Adelei", false));
+	context->Patients.push_back(new Patient(110, 33, "Erica", "Nicola", false));
+	
 	context->Departments.push_back(new Department(1, "Chirurgie"));
 }
